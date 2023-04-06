e693934f0278be60282c59f074e466d1d06aa174
Ali Raza <ali.raza@voltro.com>
2023-04-06 13:44:37 +0500

    keys path added

---
+++ build/app/generated/source/buildConfig/release/com/alchemative/outfitters/pk/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.alchemative.outfitters.pk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.alchemative.outfitters.pk";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 41;
+  public static final String VERSION_NAME = "13.2";
+}
